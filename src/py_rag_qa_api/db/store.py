74a516f5f8e97ccf98aff9a19354c56025e09f1b
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-02-21 13:18:14 +0700

    refactor: create connection pool instead of just create one connection

---
+++ src/py_rag_qa_api/db/store.py
@@ -0,0 +1,19 @@
+from py_rag_qa_api.core.config import Config
+import psycopg2.pool
+
+def createDBConnPool(cfg: Config) -> psycopg2.pool.ThreadedConnectionPool:
+    try:
+        dbConnPool = psycopg2.pool.ThreadedConnectionPool(
+            minconn=2,
+            maxconn=10,
+            user=cfg.dbUser,
+            password=cfg.dbPass,
+            host=cfg.dbHost,
+            port=cfg.dbPort,
+            dbname=cfg.dbName
+        )
+        print("connection pool ready...")
+        return dbConnPool
+    except Exception as e:
+        print(f"failed to connect to the database: {e}")
+        raise
